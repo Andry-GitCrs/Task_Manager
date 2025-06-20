ee6b3bceb88a603c97ad4ae0ceb54bc9060da6c4
Andry030 <andrianaivonoe403@gmail.com>
2025-06-20 10:25:06 +0300

    Refactor profile page: Enhance layout with cover and profile image upload features, improve styling, and add image preview functionality.

--- main/main.py
+++ main/main.py
@@ -23,13 +23,18 @@ app.config['REMEMBER_COOKIE_HTTPONLY'] = True
 app.secret_key = os.getenv("SECRET_KEY")
 port = os.getenv("PORT")
 
-login_manager = LoginManager() # Login Manager config
+# Login Manager config
+login_manager = LoginManager() 
 login_manager.login_view = 'authentication'
 login_manager.init_app(app)
-socketio = SocketIO(app, async_mode='threading') # Initialize SocketIO
-database = pg_connexion.connect(app) # Database Connection
+
+# Initialize SocketIO
+socketio = SocketIO(app, async_mode='threading')
+
+# Database Connection
+database = pg_connexion.connect(app) 
 use_app_route(app, database, login_manager, socketio) # Route
 
-## Use HTTPS
+## Run the app and use HTTPS, fake certificate
 if __name__ == "__main__":
     socketio.run(app, debug = True, port = port, host = '0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
